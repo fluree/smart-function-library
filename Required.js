722cc979c6005db852e3e632248f35160db729f3
Andrew Johnson <ajohnson.uva@gmail.com>
2020-10-14 16:39:16 -0400

    fix smart fn code typo

--- Required.js
+++ Required.js
@@ -1,13 +1,12 @@
-
-
-// For Collection spec -> certain predicates required. 
-[{
-    "_id": "_fn$required",
-    "name": "required", 
-    "params": ["predicateName"],
-    "doc": "Checks that the collection has a certain predicate",
-    "code": "(boolean (get (?e) predicateName))"
-}]
-
+// For Collection spec -> certain predicates required.
+[
+  {
+    _id: '_fn$required',
+    name: 'required',
+    params: ['predicateName'],
+    doc: 'Checks that the collection has a certain predicate',
+    code: '(boolean (get (?s) predicateName))',
+  },
+];
 
 // Example "code": "(and (required \"_collection/name\") (required \"_collection/doc\") (required \"_collection/spec\"))
